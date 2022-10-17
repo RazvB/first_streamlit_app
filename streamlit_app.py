d47f97737f6524c451af23960c1bcd0fcf36f2a0
RazvB <115987878+RazvB@users.noreply.github.com>
2022-10-17 18:15:06 +0300

    Update streamlit_app.py
    Add changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My parents new home!')
 
-streamlit.header('Ana are mere')
-streamlit.text('Mere, pere, gutui')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
